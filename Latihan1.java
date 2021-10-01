39c8be36f42276356310de4942c31ae4642e64fe
sitifardamaulina <80209705+sitifardamaulina@users.noreply.github.com>
2021-10-01 13:49:38 +0800

    Add files via upload

---
+++ Latihan1.java
@@ -0,0 +1,13 @@
+
+public class Latihan1 {
+
+    public static void main(String[] args) {
+        int uang = 50000;
+        int harga = 55000;
+        
+        String jawab = uang > harga ? uang + "Jadi Membeli" : uang + "Tidak Membeli";
+        
+        System.out.println(jawab);
+    }
+    
+}
