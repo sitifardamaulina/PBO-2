e53798abe88b9ef2afd579701433de7826353d76
sitifardamaulina <80209705+sitifardamaulina@users.noreply.github.com>
2021-10-01 13:50:52 +0800

    Add files via upload

---
+++ Latihan3_1.java
@@ -0,0 +1,17 @@
+
+public class Latihan3_1 {
+
+    
+    public static void main(String[] args) {
+       int nilai_awal = 80;
+       int nilai_akhir = 70;
+       boolean isTerbesar;
+       
+       isTerbesar = nilai_awal > nilai_akhir;
+       System.out.println("Apakah" + nilai_awal + "lebih besar dari" + nilai_akhir + "?" + isTerbesar);
+       
+       isTerbesar = nilai_awal < nilai_akhir;
+       System.out.println("Apakah" + nilai_awal + "lebih kecil dari" + nilai_akhir + "?" + isTerbesar);
+    }
+    
+}
