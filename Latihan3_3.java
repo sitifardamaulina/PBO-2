660b5fa5f8c7b623990903ba2f95829bcbd40a18
sitifardamaulina <80209705+sitifardamaulina@users.noreply.github.com>
2021-10-01 13:53:48 +0800

    Add files via upload

---
+++ Latihan3_3.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan3;
+
+
+public class Latihan3_3 {
+
+    public static void main(String[] args) {
+       
+        int number = 10;
+        
+        String jawab = number > 20? number + "Lebih besar dari 20" : number + "Lebih kecil dari 20";
+        
+        System.out.println(jawab);
+    }
+    
+}
